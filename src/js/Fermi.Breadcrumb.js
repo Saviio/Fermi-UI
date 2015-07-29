3146aba24f16c91e4e88a79ea079186f0bf2532c
思維漫走 <silver_ras@163.com>
2015-07-29 18:56:51 +0800

    upload Breadcrumb

---
+++ src/js/Fermi.Breadcrumb.js
@@ -0,0 +1,36 @@
+
+
+
+;(function(angular){
+
+    var breadcrumb=angular.module('Fermi.breadcrumb',[])
+
+    breadcrumb.directive('fermiBreadcrumb',[function(){
+
+        return {
+            restrict:'EA',
+            replace:true,
+            scope:{
+              items:'='
+            },
+            transclude:true,
+            template:`
+                <div class="breadcrumb-container">
+                    <span ng-repeat="item in items" class="breadcrumb">
+                        <span class="breadcrumb-link" ng-if="!item.label">
+                            {{item}}
+                        </span>
+                        <a  class="breadcrumb-link" ng-if="item.label" href={{item.href}}>
+                            {{item.label}}
+                        </a>
+                        <span class="breadcrumb-slash">
+                            /
+                        </span>
+                    </span>
+                </div>
+            `,
+            controller:[function(){}]
+      }
+    }])
+
+})(angular)
