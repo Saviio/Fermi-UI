2494f501bde0140d8378cd9eaeb0a5e261a2b256
Saviio <silver_Ras@163.com>
2015-07-31 00:18:53 +0800

    impl checkbox default value

--- src/js/Ferimi.Checkbox.js
+++ src/js/Ferimi.Checkbox.js
@@ -4,26 +4,28 @@
 
     var checkbox= angular.module('Fermi.checkbox',[])
 
-    checkbox.directive('fermiCheckbox',[function(){
+    checkbox.directive('fermiCheckbox',['$parse',function($parse){
         return{
             restrict:'EA',
             replace:true,
+            require: '^ngModel',
             scope:{
-                boolean:'='
+                ngModel: '='
             },
             transclude:true,
             template:`
-                <div class="checkbox" ng-click="click()" >
-                    <input type="checkbox" ng-model="boolean"></input>
+                <div class="checkbox" ng-click="ngModel=!ngModel" >
+                    <input type="checkbox" ng-model="ngModel"></input>
                     <span></span>
                 </div>
             `,
             controller:['$scope',function($scope){
-                $scope.click=function(){
-                    $scope.boolean=!$scope.boolean
-                    //alert($scope.boolean)
-                }
-            }]
+                $scope.ngModel=$scope.ngModel || false
+            }],
+            link: function (scope, element, attrs) {
+                if (attrs.ngModel && attrs.value)
+                    $parse(attrs.ngModel).assign(scope, attrs.value);
+            }
         }
     }])
 
