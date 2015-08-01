9b4c382c3aaf65a25c87f4a94d38a6bfbf506eab
Saviio <silver_Ras@163.com>
2015-08-01 20:50:06 +0800

    commit

--- src/js/Fermi.Tooltip.js
+++ src/js/Fermi.Tooltip.js
@@ -12,11 +12,36 @@
                 content:'@'
             },
             template:`
-                <div>
-                    <span>{{content}}</span>
-                </div>
-            `
+                <span ng-transclude ng-mouseover="mouseover($event)">
+                </span>
+            `,
+            controller:['$scope',function($scope){
+                $scope.container=null
+                $scope.getContainer=function(){
+                    if(!container){
+                        $scope.container=document.createElement('div')
+                        document.body.appendChild($scope.container)
+                    }
+                    return $scope.container
+                }
+
+                $scope.mouseover=function($event){
+                    
+                }
+
+            }]
         }
     }])
 
 })(angular)
+
+
+/*
+
+<div>
+    <div ng-class="{undefined:'tooltip-arrow tooltip-arrow-left',left: 'tooltip-arrow tooltip-arrow-left', right: 'tooltip-arrow tooltip-arrow-right',top:'tooltip-arrow tooltip-arrow-top',bottom:'tooltip-arrow tooltip-arrow-bottom'}[placement]"></div>
+    <div>
+        <span>{{content}}</span>
+    </div>
+</div>
+*/
