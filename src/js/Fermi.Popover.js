b5bd69871630bd9ffb10e0f0687e027849f89492
Saviio <silver_Ras@163.com>
2015-08-02 20:09:19 +0800

    commit

---
+++ src/js/Fermi.Popover.js
@@ -0,0 +1,10 @@
+;(function(angular,undefined){
+    var popover= angular.module('Fermi.popover',['Fermi.core'])
+
+    popover.directive('fermiPopover',[function(){
+        return {
+            
+        }
+    }])
+
+}(angular)
