cc31859a274e933f9f7273a5dc1f8dc97b387e1c
思維漫走 <silver_ras@163.com>
2016-01-30 01:22:09 +0800

    update

--- src/Pagination/directive/pagination.js
+++ src/Pagination/directive/pagination.js
@@ -19,47 +19,49 @@ export default class Pagination {
 
     @dependencies('$scope')
     controller(scope){
+        scope.pages = [1,2,3]
         scope.current = scope.current || 1
         scope.last = () => scope.items[scope.items.length - 1]
-        scope.next = () => scope.current++
-        scope.prev = () => scope.current--
+        scope.next = () => scope.current++ && _updatePages()
+        scope.prev = () => scope.current-- && _updatePages()
         scope.choose = item => {
             scope.current = item
+            _updatePages()
             typeof scope.onChange === 'function' && scope.onChange(item)
         }
 
-        scope.range = () => {
+        let _updatePages = () => {
             let arr = [scope.current]
             let len = scope.items.length
             let pivot = Math.round(scope.items.length / 2)
-            let i,j,f
             let num = 6
+            let f = false
 
             if(scope.current > pivot) f = true
 
             let n = f ? -1 : 1
-            for(i = 1; num >= 3 && scope.items[scope.current - n - 1] !== undefined ; i++, num--, n = f ? -i : i){
+            for(let i = 1; num > 3 && scope.items[scope.current - n - 1] !== undefined; i++, num--, n = f ? -i : i){
                 f
                 ? arr.push(scope.items[scope.current - n - 1])
                 : arr.unshift(scope.items[scope.current - n - 1])
             }
 
             let m = f ? -1 : 1
-            for(j = 1; j !== num && scope.items[scope.current + m - 1] !== undefined ; j++, m = f ? -j : j){
+            for(let j = 1; j !== num + 1 && scope.items[scope.current + m - 1] !== undefined ; j++, m = f ? -j : j){
                 f
                 ? arr.unshift(scope.items[scope.current + m - 1])
                 : arr.push(scope.items[scope.current + m - 1])
             }
-
-            return arr
+            scope.pages = arr
         }
     }
 
     link(scope, $elem, attrs, ctrl){
         let elem = $elem[0]
         let hasJumper = elem::getDOMState('jumper')
-        let mock = [1,2,3,4,5,6,7,8,9,10,11]
+        let mock = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
         scope.items = mock
+        scope.choose(scope.current)
         window.iii=scope.items
         window.ccc = scope.range
     }
--- src/core/FermiDefault.js
+++ src/core/FermiDefault.js
@@ -5,20 +5,16 @@ export default class FermiDefaultDirective{
     constructor(){
         this.restrict='A'
         this.require='^ngModel'
-        //this.controller.$inject=['$scope','$attrs', '$parse']
     }
 
     @dependencies('$scope', '$attrs', '$parse')
     controller($scope, $attrs, $parse){
-
-        var idf=$attrs.ngModel
-        var val = $scope[idf]
+        let idf = $attrs.ngModel
+        let val = $scope[idf]
         if(val == undefined){
             val = $attrs.fermiDefault
-            if(val === undefined)
-                val = $attrs.value
-            if(val !== undefined)
-                $parse($attrs.ngModel).assign($scope,JSON.parse(val))
+            if(val === undefined) val = $attrs.value
+            if(val !== undefined) $parse($attrs.ngModel).assign($scope,JSON.parse(val))
         }
     }
 }
