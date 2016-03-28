68f654cb5873cc944f9c788e11632552cbe70bff
Saviio <silver_ras@163.com>
2016-03-28 22:27:09 +0800

    update

--- src/controller/home.js
+++ src/controller/home.js
@@ -4,6 +4,7 @@ import { dependencies } from '../external/dependencies'
 export default class Home{
     constructor(timeout){
         this.lineSample = 0
+        this.popover = true
         this.timeout = timeout
         timeout(::this.showLineProgress, 2000)
     }
@@ -20,5 +21,15 @@ export default class Home{
             this.lineSample += 10
         }
         this.timeout(::this.showLineProgress, 2000)
+    }
+
+    togglePopover(){
+        if(this.popover){
+            this.popoverSample.close()
+        } else {
+            this.popoverSample.open()
+        }
+
+        this.popover = !this.popover
     }
 }
