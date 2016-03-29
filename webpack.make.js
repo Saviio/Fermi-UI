6909feb371d2acc7de239d3ec619956ed541e23f
Saviio <silver_ras@163.com>
2016-03-29 23:09:32 +0800

    update

--- webpack.make.js
+++ webpack.make.js
@@ -51,7 +51,7 @@ module.exports = function makeWebpackConfig (options) {
 
       // Output path from the view of the page
       // Uses webpack-dev-server in development
-      publicPath: BUILD ? '/' : 'http://localhost:8080/',
+      publicPath: BUILD ? '/' : 'http://127.0.0.1:8080/',
 
       // Filename for entry points
       // Only adds hash in build mode
