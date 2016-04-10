5b15e49c4870d02e15efef221a93eb00469d616a
Saviio <silver_ras@163.com>
2016-04-11 05:56:50 +0800

    update

--- src/Fermi-UI/core/RangeFilter.js
+++
@@ -1,9 +0,0 @@
-
-export default function(){
-    return (input, total) => {
-        for (var i = 0, total = parseInt(total); i < total; i++){
-            input.push(i)
-        }
-        return input
-    }
-}
--- src/Fermi-UI/utils/browser.js
+++
@@ -1,5 +0,0 @@
-const WIN = window
-const DOM = document
-const BODY = document.body
-
-export { WIN, DOM, BODY }
